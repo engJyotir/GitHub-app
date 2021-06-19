5918249f84ae28dffb1e3912a59d3896bd5fe5a1
jyotirsinha <jyotiraditya03@gmail.com>
2021-06-19 21:25:31 +0530

    gitApp

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyA3OxCiuKulYKTKnKinVxVoDeKMUuT6X5s",
+    authDomain: "mygitapp-5b156.firebaseapp.com",
+    projectId: "mygitapp-5b156",
+    storageBucket: "mygitapp-5b156.appspot.com",
+    messagingSenderId: "900416964844",
+    appId: "1:900416964844:web:1db5839874d98274abbdc9",
+    measurementId: "G-RGQB60BS1D"
+  };
